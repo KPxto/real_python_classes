36291071790c250ccca9c1c36510cfa556fe9f25
Kpxto <kaioopeixoto@gmail.com>
2023-11-01 21:35:28 -0300

    primeira atualizacao

---
+++ dash/app.py
@@ -0,0 +1,68 @@
+from dash import Dash
+from dash import dcc 
+from dash import html
+import pandas as pd
+
+
+data = pd.read_csv('avocado.csv')
+data = data.query("type == 'conventional' and region == 'Albany'")
+data['Date'] = pd.to_datetime(data['Date'], format='%Y-%m-%d')
+data.sort_values('Date', inplace=True)
+
+external_stylesheets = [
+    {
+        'href': 'https://fonts.googleapis.com/css2?'
+        'family=Lato:wght@400;700&display=swap',
+        'rel': 'stylesheet',
+    },
+]
+
+
+app = Dash(__name__)
+app.title = 'Avocado Analytics: Understand Your Avocados!'
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.P(children='', className='header-emoji'),
+                html.H1(children='Avocado Analytics',
+                className='header-title'),
+                html.P(
+                    children='Analyze the behavior of avocado prices'
+                    ' and the number of avocados sold in the US'
+                    ' between 2015 and 2018',
+                    className='header-description',
+                ),
+            ],
+            className='header',
+        ),
+        dcc.Graph(
+            figure={
+                'data': [
+                    {
+                        'x': data['Date'],
+                        'y': data['AveragePrice'],
+                        'type': 'lines',
+                    },
+                ],
+                'layout': {'title': 'Average Price of Avocados'}
+            },
+        ),
+        dcc.Graph(
+            figure={
+                'data': [
+                    {
+                        'x': data['Date'],
+                        'y': data['Total Volume'],
+                        'type': 'lines',
+                    },
+                ],
+                'layout': {'title': 'Avocados Sold'},
+            },
+        ),
+    ]
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ modules_packages_explain/code_scripts/mod.py
@@ -0,0 +1,10 @@
+s = "If I say so, please swallow it!!"
+a = [1, 2, 3]
+
+
+def foo(arg):
+    print(f'arg = {arg}')
+
+
+class Foo:
+    pass
